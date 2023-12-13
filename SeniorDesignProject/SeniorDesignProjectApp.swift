06bda0184cd3e8929b9e6a1c4830b2759a2e1365
mtshimbadi <miketshimbadi14@gmail.com>
2023-12-12 18:45:49 -0600

    Initial Commit

---
+++ SeniorDesignProject/SeniorDesignProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorDesignProjectApp.swift
+//  SeniorDesignProject
+//
+//  Created by Mike Tshimbadi on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SeniorDesignProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
